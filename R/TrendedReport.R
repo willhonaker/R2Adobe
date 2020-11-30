886e8635dec767e6caa9dc0cf13f12998bb0cf71
willhonaker <wcbhonaker@gmail.com>
2020-11-30 13:47:16 -0800

    function for 2.0 requests with time dimension + other dimension

---
+++ R/TrendedReport.R
@@ -0,0 +1,111 @@
+##
+##library(R2Adobe)
+
+
+## one dimension + one time frame + multiple metrics
+
+
+TrendedReport <- function(
+  rsid,
+  start_date,
+  end_date,
+  dimension,
+  metrics,
+  segments = NULL,
+  verbosity = FALSE,
+  progress_messages = TRUE
+){
+  
+  metric_json <- ''
+  for(i in metrics){
+    metric_json <- paste0(metric_json, '{"columnId":"',i,'", "id":"metrics/', i, '"},')
+  }
+  metric_json <- gsub(",$", "",metric_json)
+  
+  segment_json <- ''
+  for(i in segments){
+    segment_json <- paste0(segment_json, '{"type":"segment","segmentId":"',i,'"},')
+  }
+  segment_json <- gsub(",$", "",segment_json)
+  
+  days <- seq.Date(from = as.Date(start_date),
+                   to = as.Date(end_date),
+                   by = "day")
+  
+  message(paste0("Beginning queries for date range ", as.character(days[1]), " to ", as.character(days[length(days)]), "."))
+  
+  df_list <- list()
+  for(i in 1:(length(days))){
+    message(paste0("Querying data for ", days[i]))
+    
+    initial_page <- 0
+    daily_data_list <- list()
+    last_page_check = FALSE
+    
+    while(last_page_check == FALSE){
+      query <- paste0('
+        {
+          "rsid": "',rsid,'",
+          "globalFilters": [
+              ',ifelse(!is.null(segments), paste0(segment_json, ","),''),'
+              {
+                  "type": "dateRange",
+                  "dateRange": "',days[i],'T00:00:00.000/',days[i]+1,'T00:00:00.000"
+              }
+          ],
+          "metricContainer": {
+              "metrics": [',metric_json,']
+          },
+          "dimension": "variables/',dimension,'",
+          "settings": {
+            "countRepeatInstances": true,
+            "limit": 400,
+            "page": ',initial_page,',
+            "dimensionSort": "asc",
+            "nonesBehavior": "return-nones"
+          }
+        }
+      ')
+      
+      readable_response <- JWTRetry(
+        "POST",
+        url = paste0("https://analytics.adobe.io/api/", AdobeAnalytics$JWT_Credentials$company_id, "/reports"),
+        body = query,
+        body_encoding = "json",
+        times = 30
+      )
+      
+      response_df <- as.data.frame(readable_response$rows)
+      ## account for empty response here
+      
+      data <- as.data.frame(do.call(rbind, response_df$data)) 
+      data <- cbind(as.character(days[i]) ,response_df$value, data) 
+      names(data) <- c("date", dimension, readable_response$columns$columnIds)
+      daily_data_list[[initial_page+1]] <- data
+      
+      if(progress_messages == TRUE){
+        message(paste0("Pulled page ",
+                       as.character(initial_page)))
+      }
+      
+      
+      initial_page <- initial_page + 1
+      last_page_check <- readable_response$lastPage
+    }
+    df_list[[i]] <- bind_rows(daily_data_list)
+    message(paste0("Successfully queried data for ", days[i]))
+    
+  }
+  data <- do.call(rbind, df_list)
+  data
+  
+}
+  
+
+  
+ 
+
+
+
+
+
